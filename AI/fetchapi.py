52c685c18ef231534bbc799333f2f920506db9fb
ku5h4gr4 <kushagra.singh162@gmail.com>
2024-02-23 20:36:23 +0530

    updated

--- AI/fetchapi.py
+++
@@ -1,8 +0,0 @@
-def key(api):
-    with open("Api.txt", "r") as file:
-        lines = file.readlines()
-    value = ""
-    for line in lines:
-        if line.startswith(api):
-            value = line.split("=")[1].strip().strip('"')
-    return value
